891b64c16c9c8823fb4b135798ea9fc85e9c2236
Deividas Morkunas <deividasmor7@gmail.com>
2024-12-09 16:19:22 +0300

    update SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ const SearchBar = ({ text }) => {
     <div className="flex items-center gap-[1.5rem]">
       <img src="/src/assets/icon-search.svg" />
       <input
-        className="text-movie-fifth leading-[30px] text-heading-m font-medium p-0 bg-transparent border-0 border-b-2 border-movie-third"
+        className="text-movie-fifth leading-[30px] text-heading-m font-medium p-0 bg-transparent border-0 focus:border-b-[1px] focus:border-movie-third focus:ring-0 w-full"
         placeholder={text}
       />
     </div>
