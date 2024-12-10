2fe8622837ab99d22536a3fbca5a74fc624711fb
Deividas Morkunas <deividasmor7@gmail.com>
2024-12-10 12:51:20 +0300

    update style, state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
-import Homepage from "./page/Homepage";
 import RegisterPage from "./page/RegisterPage";
 import LoginPage from "./page/LoginPage";
 import TvSeriesPage from "./page/TvSeriesPage";
 import MoviesPage from "./page/MoviesPage";
 import BookmarkPage from "./page/BookmarkPage";
+import Homepage from "./page/HomePage";
 
 function App() {
   return (
     <div className="font-outfit">
       <Routes>
-    
-
         <Route path="/" element={<Homepage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,12 +1,10 @@
-import Button from "../components/Button";
 import SearchBar from "../components/SearchBar";
 
 const Homepage = () => {
   return (
     <div className="bg-movie-secondary h-[100vh]">
-      Homepage
       <SearchBar text="Search for movies or TV series" />
-      <Button>TEST</Button>
+      Homepage
     </div>
   );
 };
