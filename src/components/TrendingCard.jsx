2f7c0c1ec34ff8f235774110a3de078ab53758a0
Bernardasko <115994039+Bernardasko@users.noreply.github.com>
2024-12-12 18:09:53 +0200

    fixed trending cards

--- src/components/TrendingCard.jsx
+++ src/components/TrendingCard.jsx
@@ -25,30 +25,25 @@ export default function TrendingCard({ content }) {
     );
   };
   return (
-    <>
-      <picture>
-        <source
-          media="(max-width: 640px)"
-          srcSet={`src/${content.thumbnail.trending.small.substring(1)}`}
-        />
-        <img
-          className="w-[15rem] sm:w-[29.375rem] lg:w-[29.375rem]"
-          src={`src/${content.thumbnail.trending.large.substring(1)}`}
-          alt={content.title}
-        />
-      </picture>
-      <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent py-[0.99rem] px-4 sm:py-[1.3rem] sm:px-6 lg:py-5 lg:px-[1.5rem] flex flex-col justify-end ">
-        <div className="text-xs sm:text-[0.9375rem] opacity-75 flex items-center gap-[0.39rem] sm:gap-[0.4rem] lg:gap-[0.4rem]">
-          <span>{content.year}</span>
-          <span className="px-[0.1rem] sm:px-[0.04rem]">•</span>
-          {displayIcon(content.category)}
-          <span>{content.category}</span>
-          <span className="px-[0.1rem] sm:px-[0.05rem] ">•</span>
-          <span>{content.rating}</span>
+    <div className="relative">
+      <div className="group relative">
+        <picture>
+          <source
+            media="(max-width: 640px)"
+            srcSet={`src/${content.thumbnail.trending.small.substring(1)}`}
+          />
+          <img
+            className="w-[15rem] sm:w-[29.375rem] lg:w-[29.375rem]"
+            src={`src/${content.thumbnail.trending.large.substring(1)}`}
+            alt={content.title}
+          />
+        </picture>
+        <div className="absolute inset-0 hidden group-hover:flex items-center justify-center opacity-0 hover:opacity-100 bg-opacity-50 bg-black transition">
+          <button className="bg-white bg-opacity-25 text-white rounded-full p-2 flex items-center space-x-2">
+            <img src="src/assets/icon-play.svg" alt="Play icon" />
+            <p className="text-heading-xs pl-2 pr-4">Play</p>
+          </button>
         </div>
-        <h3 className="relative top-0.5 sm:top-0 text-[0.9375rem] sm:text-[1.5rem]">
-          {content.title}
-        </h3>
       </div>
       <div className="group">
         <button
@@ -66,6 +61,19 @@ export default function TrendingCard({ content }) {
           </svg>
         </button>
       </div>
-    </>
+      <div className="sticky inset-0 bg-gradient-to-t from-black via-transparent to-transparent py-[0.99rem] px-4 sm:py-[1.3rem] sm:px-6 lg:py-5 lg:px-[1.5rem] flex flex-col justify-end ">
+        <div className="text-xs sm:text-[0.9375rem] opacity-75 flex items-center gap-[0.39rem] sm:gap-[0.4rem] lg:gap-[0.4rem]">
+          <span>{content.year}</span>
+          <span className="px-[0.1rem] sm:px-[0.04rem]">•</span>
+          {displayIcon(content.category)}
+          <span>{content.category}</span>
+          <span className="px-[0.1rem] sm:px-[0.05rem] ">•</span>
+          <span>{content.rating}</span>
+        </div>
+        <h3 className="relative top-0.5 sm:top-0 text-[0.9375rem] sm:text-[1.5rem]">
+          {content.title}
+        </h3>
+      </div>
+    </div>
   );
 }
