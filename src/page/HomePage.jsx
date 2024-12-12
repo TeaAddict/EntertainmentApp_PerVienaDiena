bb406e09023a06c7dde39e7cd03a4bffb715a63f
Deividas Morkunas <deividasmor7@gmail.com>
2024-12-12 11:33:16 +0300

    adding homepage search state

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import RecomendedSection from "../components/RecomendedSection";
 import SearchBar from "../components/SearchBar";
 import Trending from "../components/Trending";
 
 const HomePage = ({ movies }) => {
+  const [searchText, setSearchText] = useState("");
+
   const trendingMovies = movies.filter((movie) => movie.isTrending);
 
   if (!movies) return <p>Loading data...</p>;
+
+  console.log("Search val: ", searchText);
   return (
     <div className="bg-movie-secondary flex flex-col gap-[1.5rem] md:gap-0">
       <div className="mt-[1.5rem] md:my-[2.06rem]">
-        <SearchBar placeholderText="Search for movies or TV series" />
+        <SearchBar
+          placeholderText="Search for movies or TV series"
+          setValue={setSearchText}
+        />
       </div>
       <div className="md:mb-[2.44rem]">
         <Trending data={trendingMovies} />
