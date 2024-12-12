5828ab3b6faf2f6ed16a312b1d8b4db1d9bce162
[Nedas Jurk] <[dead.nedas@gmail.com]>
2024-12-12 13:23:04 +0200

    small padding fixes

--- src/components/ContentSection.jsx
+++ src/components/ContentSection.jsx
@@ -3,7 +3,7 @@ import ContentCard from "./ContentCard";
 import { getMovies } from "../helpers/movies/get";
 import { v4 as uuidv4 } from "uuid";
 
-export default function ContentSection({ movies, heading }) {
+export default function ContentSection({ movies }) {
   const [data, setData] = useState(movies);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -32,11 +32,14 @@ export default function ContentSection({ movies, heading }) {
 
   return (
     <div className="bg-movie-secondary">
-      <h2 className="text-[1.2rem] mb-[0.7rem] sm:text-[1.91rem] lg:text-[1.91rem] sm:mb-[1.2rem] text-movie-fifth">
-        {heading}
-      </h2>
+      <h1 className="text-[1.2rem] mb-[0.7rem] md:text-[1.91rem] desktop:text-[1.91rem] desktop:mb-[28px] desktop:tracking-[0.3px]">
+        Recommended for you
+      </h1>
 
-      <ul className="grid grid-cols-2 md:grid-cols-3 desktop:grid-cols-4 gap-x-[0.938rem] gap-y-[1rem] md:gap-x-[1.813rem] md:gap-y-[1.5rem] desktop:gap-x-[2.5rem] desktop:gap-y-[2rem]">
+      <ul
+        className="grid grid-cols-2 md:grid-cols-3 desktop:grid-cols-4 gap-x-[0.938rem] gap-y-[1rem] md:gap-x-[1.813rem] md:gap-y-[1.5rem] 
+      desktop:gap-x-[2.5rem] desktop:gap-y-[2rem]"
+      >
         {data.map(
           (content) =>
             !content.isTrending && (
