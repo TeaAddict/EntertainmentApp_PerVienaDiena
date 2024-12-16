3aa25409dc4bdfa08e2b1251559124149cf385aa
Deividas Morkunas <deividasmor7@gmail.com>
2024-12-16 14:22:54 +0300

    fix missing page button

--- src/page/ErrorPage.jsx
+++ src/page/ErrorPage.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router";
+import Button from "../components/Button";
 
 const ErroePage = () => {
   return (
     <section className="h-[100vh]  bg-movie-fourth">
       <section className=" flex flex-col justify-center items-center text-movie-fifth h-[100%] gap-[5rem] ">
         <h1 className=" text-[8rem]">404 - Page Not Found</h1>
-          <Link to="/" className=" text-[2rem] rounded-[1rem] p-2 bg-movie-primary" > Go Home Page</Link>
+        <Link to="/" tabIndex={-1}>
+          <Button>Go Home Page</Button>
+        </Link>
       </section>
     </section>
   );
