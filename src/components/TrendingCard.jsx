e7f13eef2526327cecd6e778266a188dd9b0f90e
Bernardasko <115994039+Bernardasko@users.noreply.github.com>
2024-12-16 14:24:10 +0200

    fix trendings header perfect

--- src/components/TrendingCard.jsx
+++ src/components/TrendingCard.jsx
@@ -26,19 +26,19 @@ export default function TrendingCard({ content }) {
   };
   return (
     <div className="relative">
-      <div className="group relative">
+      <div className="group relative bg-cover bg-no-repeat bg-center">
         <picture>
           <source
             media="(max-width: 640px)"
             srcSet={`src/${content.thumbnail.trending.small.substring(1)}`}
           />
           <img
-            className="w-[15rem] sm:w-[29.375rem] lg:w-[29.375rem]"
+            className="w-[15rem] md:w-[29.375rem] desktop:w-[29.38rem] "
             src={`src/${content.thumbnail.trending.large.substring(1)}`}
             alt={content.title}
           />
         </picture>
-        <div className="absolute inset-0 hidden group-hover:flex items-center justify-center opacity-0 hover:opacity-100 bg-opacity-50 bg-black transition">
+        <div className="absolute inset-0  hidden group-hover:flex items-center justify-center opacity-0 hover:opacity-100 bg-opacity-50 bg-black transition">
           <button className="bg-white bg-opacity-25 text-white rounded-full p-2 flex items-center space-x-2">
             <img src="src/assets/icon-play.svg" alt="Play icon" />
             <p className="text-heading-xs pl-2 pr-4">Play</p>
@@ -47,7 +47,7 @@ export default function TrendingCard({ content }) {
       </div>
       <div className="group">
         <button
-          className="absolute desktop:top-[16px] top-[8px] desktop:right-[24px] right-[8px] w-8 h-8 sm:right-[24px] sm:top-[16px] bg-movie-secondary bg-opacity-50 rounded-full flex items-center justify-center hover:bg-movie-fifth transition"
+          className="absolute desktop:top-[16px] top-[8px] desktop:right-[24px] right-[8px] w-8 h-8 md:right-[24px] md:top-[16px] bg-movie-secondary bg-opacity-50 rounded-full flex items-center justify-center hover:bg-movie-fifth transition"
           onClick={handleBookmarkToggle}
         >
           <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg">
@@ -61,16 +61,16 @@ export default function TrendingCard({ content }) {
           </svg>
         </button>
       </div>
-      <div className="sticky inset-0 bg-gradient-to-t from-black via-transparent to-transparent py-[0.99rem] px-4 sm:py-[1.3rem] sm:px-6 lg:py-5 lg:px-[1.5rem] flex flex-col justify-end ">
-        <div className="text-xs sm:text-[0.9375rem] opacity-75 flex items-center gap-[0.39rem] sm:gap-[0.4rem] lg:gap-[0.4rem]">
+      <div className="sticky bg-gradient-custom bottom-0 py-[0.99rem] px-4 md:py-[1.3rem] md:px-6  desktop:px-[1.5rem] desktop:py-[1.31rem] flex flex-col justify-end ">
+        <div className="text-xs sm:text-[0.9375rem] opacity-75 flex items-center gap-[0.39rem] md:gap-[0.4rem] desktop:gap-[0.4rem]">
           <span>{content.year}</span>
-          <span className="px-[0.1rem] sm:px-[0.04rem]">•</span>
+          <span className="px-[0.1rem] md:px-[0.04rem] ">•</span>
           {displayIcon(content.category)}
           <span>{content.category}</span>
-          <span className="px-[0.1rem] sm:px-[0.05rem] ">•</span>
+          <span className="px-[0.1rem] md:px-[0.05rem] ">•</span>
           <span>{content.rating}</span>
         </div>
-        <h3 className="relative top-0.5 sm:top-0 text-[0.9375rem] sm:text-[1.5rem]">
+        <h3 className="relative top-0.5 md:top-0 text-[0.9375rem] md:text-[1.5rem]">
           {content.title}
         </h3>
       </div>
