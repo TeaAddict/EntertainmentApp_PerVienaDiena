df64a4124b0caa9e5c8f6b53aab44dc1e56b3dd2
Deividas Morkunas <deividasmor7@gmail.com>
2024-12-17 10:06:02 +0300

    make looking glass icon be able to select input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,13 +9,17 @@ const SearchBar = ({ placeholderText, setValue }) => {
   };
 
   return (
-    <div className="flex items-start gap-[1rem] w-full md:gap-[1.5rem] p-[1px]">
+    <label
+      htmlFor="search"
+      className="flex items-start gap-[1rem] w-full md:gap-[1.5rem] p-[1px]"
+    >
       <img
         src="/src/assets/icon-search.svg"
         className="w-[24px] h-[24px] md:w-[32px] md:h-[32px]"
       />
       <div className="relative w-full">
         <input
+          id="search"
           className="peer placeholder:text-slate-400 text-movie-fifth leading-[20px] md:leading-[30px] text-[16px] md:text-heading-m font-medium p-0 bg-transparent  focus:ring-0 w-full border-none caret-movie-primary"
           placeholder={placeholderText}
           onChange={handleChange}
@@ -23,7 +27,7 @@ const SearchBar = ({ placeholderText, setValue }) => {
         />
         <div className="absolute top-0 h-[2.44rem] w-full  peer-focus:border-b-[1px] peer-focus:border-movie-third pointer-events-none" />
       </div>
-    </div>
+    </label>
   );
 };
 
